7c2b31a969d3d73ac74baaa9e7e402bc51178272
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-15 21:43:52 -0300

    feat: create a checkbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SearchIcon from "@/assets/icons/search.svg?react"
+import { Checkbox } from "@/components/checkbox";
 import { TextField } from "@/components/text-field";
 
 export function App() {
 	return (
 		<div>
 			<TextField placeholder="Placeholder" leadingIcon={SearchIcon}/>
+			<Checkbox />
 		</div>
 	)
 }
---
+++ src/components/checkbox.tsx
@@ -0,0 +1,33 @@
+import { type ComponentProps, useId } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+import CheckIcon from "@/assets/icons/check.svg?react"
+import { Icon } from "@/components/icon";
+
+const checkboxWrapperVariants = tv({
+    base: "rounded-xs border border-border-primary has-checked:border-accent-brand has-checked:bg-accent-brand grid place-items-center group hover:border-border-active",
+    variants: {
+        size: {
+            md: "size-4"
+        }
+    },
+    defaultVariants: {
+        size: "md"
+    }
+})
+
+interface CheckboxProps extends Omit<ComponentProps<'input'>, "size">, VariantProps<typeof checkboxWrapperVariants> { }
+export function Checkbox({ id, size, ...inputProps }: CheckboxProps) {
+    const defaultId = useId()
+    id = id ?? defaultId
+
+    return (
+        <label htmlFor={id} className={checkboxWrapperVariants({ size })}>
+            <input id={id} type="checkbox" {...inputProps} className="appearance-none peer" />
+            <Icon
+                svg={CheckIcon}
+                className="fill-white size-3 not-peer-checked:hidden"
+            />
+        </label>
+    )
+}
