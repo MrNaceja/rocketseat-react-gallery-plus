dd0421e7e8465393ac3217104623c97f2a2dba99
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-19 20:21:56 -0300

    feat: create a with debounce utility

---
+++ src/utils/with-debounce.ts
@@ -0,0 +1,16 @@
+export const withDebounce = <T extends (...args: unknown[]) => void>(
+  fn: T,
+  delay: number = 2000
+) => {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+
+  return (...args: Parameters<T>) => {
+    if (timeout !== undefined) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      timeout = undefined;
+      fn(...args);
+    }, delay);
+  };
+};
