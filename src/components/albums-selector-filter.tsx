5bba72c9a3da3e8c6927320e0be68c9a0610c959
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-26 19:46:33 -0300

    feat: add a skeleton albums filter loading on index

--- src/components/albums-selector-filter.tsx
+++ src/components/albums-selector-filter.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Text } from "@/components/ui/text";
 import { useFetchAlbumsQuery } from "@/hooks/use-fetch-albums-query";
 import { Route as IndexRoute } from "@/routes"
 
 export function AlbumsSelectorFilter() {
-    const { albums } = useFetchAlbumsQuery()
+    const { albums, isLoading: isLoadingAlbums } = useFetchAlbumsQuery()
     const { albumId } = IndexRoute.useSearch()
 
     return (
@@ -12,19 +13,32 @@ export function AlbumsSelectorFilter() {
             <Text variant="heading-small" className="text-heading">Álbuns</Text>
 
             <ul className="flex items-center overflow-x-auto gap-3">
-                <Button variant={!albumId ? "primary" : "ghost"} size="sm">
-                    <IndexRoute.Link to={IndexRoute.to}>
-                        Todos
-                    </IndexRoute.Link>
-                </Button>
                 {
-                    albums.map((album) => (
-                        <Button variant={albumId === album.id ? "primary" : "ghost"} size="sm" key={album.id}>
-                            <IndexRoute.Link to={IndexRoute.to} search={{ albumId: album.id }}>
-                                {album.title}
-                            </IndexRoute.Link>
-                        </Button>
-                    ))
+                    isLoadingAlbums
+                        ? (
+                            Array.from({ length: 5 }).map((_, idx) => (
+                                <Skeleton key={idx} rounded="sm" className="h-7 w-20" />
+                            ))
+                        )
+                        : (
+                            <>
+                                <Button variant={!albumId ? "primary" : "ghost"} size="sm">
+                                    <IndexRoute.Link to={IndexRoute.to}>
+                                        Todos
+                                    </IndexRoute.Link>
+                                </Button>
+                                {
+                                    albums.map((album) => (
+                                        <Button variant={albumId === album.id ? "primary" : "ghost"} size="sm" key={album.id}>
+                                            <IndexRoute.Link to={IndexRoute.to} search={{ albumId: album.id }}>
+                                                {album.title}
+                                            </IndexRoute.Link>
+                                        </Button>
+                                    ))
+                                }
+                            </>
+
+                        )
                 }
             </ul>
         </div>
